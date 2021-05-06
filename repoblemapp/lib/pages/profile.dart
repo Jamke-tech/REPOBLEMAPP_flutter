9b6fe3076b52f10b71c947a11e471a9139fde9a1
MariaCaliz <mariacalizgonzalez@gmail.com>
2021-05-07 01:01:47 +0200

    profile_change

--- repoblemapp/lib/pages/profile.dart
+++ repoblemapp/lib/pages/profile.dart
@@ -1,11 +1,13 @@
 //import 'dart:ffi';
 
+import 'package:flash/flash.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:repoblemapp/models/User.dart';
 import 'package:repoblemapp/services/user_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:repoblemapp/widgets/error_toast.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -53,11 +55,97 @@ class _ProfileState extends State<Profile> {
                         height: 10.0,
                       ),
                       Text("userDetails['name']",
-                          style: TextStyle(fontSize: 22.0, color: Colors.white))
+                          style:
+                              TextStyle(fontSize: 22.0, color: Colors.white)),
+                      Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.teal[900],
+                                  elevation: 5,
+                                  padding: EdgeInsets.all(15),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  if (userDetails != null) {
+                                    Navigator.pushNamed(
+                                        context, '/edit_profile',
+                                        arguments: {
+                                          'map': userDetails,
+                                        });
+                                  } else {
+                                    showFlash(
+                                        context: context,
+                                        duration: const Duration(seconds: 3),
+                                        builder: (context, controller) {
+                                          return ErrorToast(
+                                            controller: controller,
+                                            textshow: "Servidor no disponible",
+                                          );
+                                        });
+                                  }
+                                },
+                                child: Text(
+                                  'Editar',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.teal[50],
+                                  ),
+                                )),
+                          ))
                     ]),
               ),
             ),
+          ),
+          Container(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "User Name:",
+                    style: TextStyle(
+                        color: Colors.redAccent,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 28.0),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "User Name:",
+                    style: TextStyle(
+                        color: Colors.redAccent,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 28.0),
+                  ),
+                ],
+              ),
+            ),
           )
+          /*ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Container(
+                height: 50,
+                child: const Center(child: Text('User Name')),
+              ),
+              const Divider(
+                height: 2,
+                thickness: 2,
+              ),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Lo que sea')),
+              ),
+            ],
+          ),*/
         ],
       ),
     );
