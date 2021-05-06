8b0e6c862f4d3621c4e3201cdebb40f1b0b5a544
gerardbelvis <gerard.belvis@gmail.com>
2021-05-06 10:05:34 +0200

    Ofertas favoritas

---
+++ repoblemapp/lib/pages/fav_offers.dart
@@ -0,0 +1,18 @@
+import 'package:flash/flash.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:repoblemapp/models/User.dart';
+import 'package:repoblemapp/services/user_service.dart';
+import 'package:repoblemapp/widgets/error_toast.dart';
+
+class Fav extends StatefulWidget {
+  @override
+  _FavState createState() => _FavState();
+}
+
+class _FavState extends State<Fav> {
+ @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
