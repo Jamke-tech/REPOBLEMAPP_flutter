24f4de2a392fbc6e9f2402fd1e9f6cea72eb0e40
AbelOrPa <abel.ortiz@estudiantat.upc.edu>
2021-05-19 11:18:16 +0200

    Segumos progresando chat

--- repoblemapp/lib/pages/xat_page.dart
+++ repoblemapp/lib/pages/xat_page.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:repoblemapp/models/User.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:socket_io/socket_io.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+
 
 class ChatPage extends StatefulWidget {
   @override
@@ -6,12 +14,34 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  
+  final messageController = TextEditingController();
+  User currentUser;
+  List<String> messages;
+  SocketIO socketIO;
+
+  
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> infoOfChat;
     Map data = ModalRoute.of(context).settings.arguments;
     infoOfChat =
-        data['map']; //Sacamos de los argumentos la información del usuario
+        data['map']; 
+    //Sacamos de los argumentos la información del usuario
+    var currentUser = infoOfChat['talkers'][];
+
+    socketIO = SocketIOManager().createSocketIO(
+        '<ENTER_YOUR_SERVER_URL_HERE>', '/',
+        query: 'chatID=${infoOfChat['chatId']}');
+    socketIO.init();
+
+    socketIO.subscribe('receive_message', (jsonData) {
+      Map<String, dynamic> data = json.decode(jsonData);
+      messages.add(data['content']);
+      notifyListeners();
+    });
+
+    socketIO.connect();
 
     return Scaffold(
       appBar: AppBar(
@@ -28,7 +58,7 @@ class _ChatPageState extends State<ChatPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(infoOfChat['Offer'], style: TextStyle(fontSize: 16)),
-                Text(infoOfChat['user'], style: TextStyle(fontSize: 12))
+                Text(infoOfChat['Offer']['owner'], style: TextStyle(fontSize: 12))
               ],
             )
           ],
@@ -43,7 +73,7 @@ class _ChatPageState extends State<ChatPage> {
           Spacer(),
           Expanded(
               child: ListView.builder(
-            itemBuilder: (context, index) => Message(message: infoOfChat['messages'][index ],),
+            itemBuilder: (context, index) => Message(message: infoOfChat['messages'][index],),
           )),
           Container(
             padding: EdgeInsets.symmetric(
@@ -80,7 +110,9 @@ class _ChatPageState extends State<ChatPage> {
                         SizedBox(width: 5),
                         Expanded(
                             child: TextField(
+                                controller: messageController,
                                 decoration: InputDecoration(
+
                                     hintText: "Escriu un missatge",
                                     border: InputBorder.none))),
                         Icon(
@@ -113,6 +145,21 @@ class _ChatPageState extends State<ChatPage> {
       ),
     );
   }
+  void sendMessage(String text, String senderChatId) {
+    messages.add(text);
+    socketIO.sendMessage(
+      'send_message',
+      json.encode({
+        'senderChatID': senderChatId,
+        'content': text,
+      }),
+    );
+    notifyListeners();
+  }
+  List<String> getMessagesForChat(String chatID) {
+    return messages
+        .toList();
+  }
 }
 
 class Message extends StatelessWidget {
@@ -144,4 +191,5 @@ class Message extends StatelessWidget {
       ],
     );
   }
+   
 }
