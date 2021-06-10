fd087680d05b0b52bca718665051185ca995a35d
miguel08171099 <71837104+miguel08171099@users.noreply.github.com>
2021-06-10 11:28:32 +0200

    setState 2

--- repoblemapp/lib/pages/my_offers.dart
+++ repoblemapp/lib/pages/my_offers.dart
@@ -14,16 +14,19 @@ class MyOffers extends StatefulWidget {
 
 class _MyOffersState extends State<MyOffers> {
   get infoOfOffers => null;
-  List<Offer> createdOffers;
+  List<dynamic> createdOffers = [];
 
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> infoOfOffers;
+    Map<String, dynamic> infoOfUser;
     Map data = ModalRoute.of(context).settings.arguments;
-    infoOfOffers = data['map'];
+    infoOfUser = data['map'];
     print("my_offers");
-    print(infoOfOffers);
-    int numberOfMyOffers = infoOfOffers['createdOffers'].length; //COMPROBAR ESTA LÍNEA DE CÓDIGO SI FALLA ALGO DE MIS OFERTAS
+    print(infoOfUser);
+    if (createdOffers.isEmpty) {
+      createdOffers = infoOfUser['createdOffers'];
+    }
+    int numberOfMyOffers = createdOffers.length; //COMPROBAR ESTA LÍNEA DE CÓDIGO SI FALLA ALGO DE MIS OFERTAS
     print(numberOfMyOffers);
     return Scaffold(
         appBar: AppBar(
@@ -58,140 +61,157 @@ class _MyOffersState extends State<MyOffers> {
                 itemCount: numberOfMyOffers,
                 scrollDirection: Axis.vertical,
                 itemBuilder: (context, index) {
-                  Map infoOffer;
+                  Offer infoOffer = createdOffers[index];
                   return Card(
-                        margin: EdgeInsets.only(right: 22.0),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.teal,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                        //para darle un poco de profundidad
-                        elevation: 5.0,
-                        shadowColor: Color(0x55434343),
-                        child: InkWell(
-                          onTap: () async {
-                            //Fem funció per anar a la info de una oferta
-                            //Li pasem el mapa de la oferta completa a la ruta per mostrar l'oferta
-                            //Hem de recollir qui es el owner de la oferta per passar a la info de la oferta
-                            UsersManager manager = UsersManager.getInstance();
-                            Map infoOfOwner = await manager.getOwner(infoOffer['owner']);
-                            Navigator.pushNamed(context, '/infoActivity',
-                                arguments: {'mapOffer': infoOffer, 'mapOwner': infoOfOwner});
-                          },
-                          child: Container(
-                            child: Padding(
-                              padding: EdgeInsets.all(12.0),
-                              child: Column(
-                                //nos lo alinea arriba:
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                    margin: EdgeInsets.only(right: 22.0),
+                    clipBehavior: Clip.antiAlias,
+                    color: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    //para darle un poco de profundidad
+                    elevation: 5.0,
+                    shadowColor: Color(0x55434343),
+                    child: InkWell(
+                      onTap: () async {
+                        //Fem funció per anar a la info de una oferta
+                        //Li pasem el mapa de la oferta completa a la ruta per mostrar l'oferta
+                        //Hem de recollir qui es el owner de la oferta per passar a la info de la oferta
+                        UsersManager manager = UsersManager.getInstance();
+                        Map infoOfOwner =
+                            await manager.getOwner(createdOffers[index].owner);
+                        Navigator.pushNamed(context, '/infoActivity',
+                            arguments: {
+                              'mapOffer': infoOffer,
+                              'mapOwner': infoOfOwner
+                            });
+                      },
+                      child: Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Column(
+                            //nos lo alinea arriba:
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
                                 children: [
-                                  Row(
-                                    children: [
-                                      Expanded(
-                                        child: Column(
-                                          //Queremos el title y la ubicacion
-                                          mainAxisAlignment: MainAxisAlignment.end,
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              infoOffer["title"],
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 22.0,
-                                                fontWeight: FontWeight.w800,
-                                              ),
-                                            ),
-                                            SizedBox(height: 3.0),
-                                            Text(
-                                              infoOffer["village"],
-                                              textAlign: TextAlign.end,
-                                              style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 17.0,
-                                                fontWeight: FontWeight.w200,
-                                              ),
-                                            ),
-                                          ],
+                                  Expanded(
+                                    child: Column(
+                                      //Queremos el title y la ubicacion
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          infoOffer.title,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w800,
+                                          ),
                                         ),
-                                      ),
-                                      // Poner fotos en la lista de ofertas
-                                      /*Expanded(
+                                        SizedBox(height: 3.0),
+                                        Text(
+                                          infoOffer.village,
+                                          textAlign: TextAlign.end,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 17.0,
+                                            fontWeight: FontWeight.w200,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  // Poner fotos en la lista de ofertas
+                                  /*Expanded(
                                         child: DecoratedBox(
                                           decoration: BoxDecoration(
                                           image: DecorationImage(image: infoOffer["pictures"][0])
                                           ),
                                         )
                                         ),*/
-                                      Expanded(
-                                          child: Column(
-                                              mainAxisAlignment: MainAxisAlignment.end,
-                                              crossAxisAlignment: CrossAxisAlignment.end,
-                                              children: [
-                                            IconButton(
-                                              icon: Icon(
-                                                Icons.remove_red_eye,
-                                                color: Colors.white,
-                                                size: 30,
-                                              ),
-                                              onPressed: () async {
-                                                UsersManager manager = UsersManager.getInstance();
-                                                Map infoOfOwner =
-                                                    await manager.getOwner(infoOffer['owner']);
-                                                Navigator.pushNamed(context, '/infoActivity',
-                                                    arguments: {
-                                                      'mapOffer': infoOffer,
-                                                      'mapOwner': infoOfOwner
+                                  Expanded(
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.end,
+                                          children: [
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.remove_red_eye,
+                                            color: Colors.white,
+                                            size: 30,
+                                          ),
+                                          onPressed: () async {
+                                            UsersManager manager =
+                                                UsersManager.getInstance();
+                                            Map infoOfOwner = await manager
+                                                .getOwner(infoOffer.owner);
+                                            Navigator.pushNamed(
+                                                context, '/infoActivity',
+                                                arguments: {
+                                                  'mapOffer': infoOffer,
+                                                  'mapOwner': infoOfOwner
+                                                });
+                                          },
+                                        ),
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.create,
+                                            color: Colors.white,
+                                            size: 30,
+                                          ),
+                                          onPressed: () async {
+                                            Navigator.pushNamed(
+                                                context, '/update_offer',
+                                                arguments: {
+                                                  'mapOffer': infoOffer
+                                                });
+                                            print("Modificar clicado");
+                                            print(infoOffer);
+                                          },
+                                        ),
+                                        IconButton(
+                                            icon: Icon(
+                                              Icons.delete,
+                                              color: Colors.white,
+                                              size: 30,
+                                            ),
+                                            onPressed: () async {
+                                              OffersManager manager =
+                                                  OffersManager.getInstance();
+                                              String code =
+                                                  await manager.deleteOffer(
+                                                      createdOffers[index]['_id']);
+                                              if (code == "200") {
+                                                print("Oferta eliminada");
+                                                _getListMyOffers();
+                                              } else {
+                                                showFlash(
+                                                    context: context,
+                                                    duration: const Duration(
+                                                        seconds: 3),
+                                                    builder:
+                                                        (context, controller) {
+                                                      return ErrorToast(
+                                                        controller: controller,
+                                                        textshow:
+                                                            "Servidor no disponible",
+                                                      );
                                                     });
-                                              },
-                                            ),
-                                            IconButton(
-                                              icon: Icon(
-                                                Icons.create,
-                                                color: Colors.white,
-                                                size: 30,
-                                              ),
-                                              onPressed: () async {
-                                                Navigator.pushNamed(context, '/update_offer',
-                                                    arguments: {'mapOffer': infoOffer});
-                                                print("Modificar clicado");
-                                                print(infoOffer);
-                                              },
-                                            ),
-                                            IconButton(
-                                                icon: Icon(
-                                                  Icons.delete,
-                                                  color: Colors.white,
-                                                  size: 30,
-                                                ),
-                                                onPressed: () async {
-                                                  OffersManager manager =
-                                                      OffersManager.getInstance();
-                                                  String code =
-                                                      await manager.deleteOffer(infoOffer['_id']);
-                                                  if (code == "200") {
-                                                    print("Oferta eliminada");
-                                                    _getListMyOffers();
-                                                  } else {
-                                                    showFlash(
-                                                        context: context,
-                                                        duration: const Duration(seconds: 3),
-                                                        builder: (context, controller) {
-                                                          return ErrorToast(
-                                                            controller: controller,
-                                                            textshow: "Servidor no disponible",
-                                                          );
-                                                        });
-                                                  }
-                                                })
-                                          ]))
-                                    ],
-                                  )
+                                              }
+                                            })
+                                      ]))
                                 ],
-                              ),
-                            ),
+                              )
+                            ],
                           ),
                         ),
-                      );
+                      ),
+                    ),
+                  );
                 },
               )
                   //child: myOfferCard(infoOffer: infoOfOffers['createdOffers'][0], rating: 2) //Debería funcionar con lo comentado
@@ -202,7 +222,7 @@ class _MyOffersState extends State<MyOffers> {
   /*Widget _itemBuilder(BuildContext context, int index) {
     return myOfferCard(infoOffer: infoOfOffers['createdOffers'][index], rating: 1);
   }*/
-   void _getListMyOffers() async {
+  void _getListMyOffers() async {
     UsersManager manager = UsersManager.getInstance();
     Map infoOfUser = await manager.getUser();
 
